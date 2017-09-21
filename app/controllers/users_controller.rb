78934d6566c454a0e990de437dbf06d4c06fadbc
Jackie Ellenberger <jackie.ellenberger@gmail.com>
2017-09-21 16:57:53 -0700

    Add 5 repos in alphabetical order to show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
   end
 
   def show
+    github = Github.new
+
+    @user_repos = github.repos.list(user: params[:user])
+    .map.with_index { |x,i| if i < 5 then x end }
+
+    @user = params[:user]
+    @user_repos = @user_repos.as_json
   end
 end
