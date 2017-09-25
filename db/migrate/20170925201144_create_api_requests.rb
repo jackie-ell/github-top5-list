ece184f7b9178993a5ea70cadc6ced94f589e3ba
Jackie Ellenberger <jackie.ellenberger@gmail.com>
2017-09-25 14:09:54 -0700

    Remove ApiRequest model

--- db/migrate/20170925201144_create_api_requests.rb
+++
@@ -1,9 +0,0 @@
-class CreateApiRequests < ActiveRecord::Migration[5.1]
-  def change
-    create_table :api_requests do |t|
-      t.timestamps null: false
-      t.text :url, null: false
-    end
-    add_index :api_requests, :url, unique: true
-  end
-end
